2c86eb22e54777f5cc22cfb682d94baf49d6ec92
Lukas Petry <git.luke@rennwurst.de>
2023-10-14 18:20:46 +0200

    :bug: Pin onload too

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
-import {Plugin} from 'obsidian';
+import { Plugin } from 'obsidian';
 
 const dateRegex = /\d{4}-\d{2}-\d{2}/;
 
 export default class DailyNotePinnerPlugin extends Plugin {
 	async onload() {
 		this.registerEvent(
-			this.app.workspace.on("file-open", () => {
-				const today = new Date().toISOString().substring(0, 10);
-
-				this.app.workspace.iterateRootLeaves(leaf => {
-					if (dateRegex.test(leaf.view.file.basename)) {
-						const shouldBePinned = leaf.view.file.basename === today;
-						if (leaf.pinned !== shouldBePinned) {
-							leaf.setPinned(shouldBePinned);
-						}
-					}
-				})
-			})
+			this.app.workspace.on("file-open", this.pinDailyNote)
 		);
+		this.pinDailyNote();
+	}
+
+	pinDailyNote = () => {
+		const today = new Date().toISOString().substring(0, 10);
+		this.app.workspace.iterateRootLeaves(leaf => {
+			if (dateRegex.test(leaf.view.file?.basename)) {
+				const shouldBePinned = leaf.view.file?.basename === today;
+				if (leaf?.pinned !== shouldBePinned) {
+					leaf.setPinned(shouldBePinned);
+				}
+			}
+		})
 	}
 }
