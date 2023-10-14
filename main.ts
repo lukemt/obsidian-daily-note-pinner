921de85666bfa0982af18771b119c852d3e5b427
Lukas Petry <git.luke@rennwurst.de>
2023-10-14 19:13:42 +0200

    :green_heart: Fix TypeScript errors

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { Plugin, FileView } from 'obsidian';
 
 const dateRegex = /\d{4}-\d{2}-\d{2}/;
 
@@ -13,9 +13,9 @@ export default class DailyNotePinnerPlugin extends Plugin {
 	pinDailyNote = () => {
 		const today = new Date().toISOString().substring(0, 10);
 		this.app.workspace.iterateRootLeaves(leaf => {
-			if (dateRegex.test(leaf.view.file?.basename)) {
+			if (leaf.view instanceof FileView && dateRegex.test(leaf.view.file?.basename ?? "")) {
 				const shouldBePinned = leaf.view.file?.basename === today;
-				if (leaf?.pinned !== shouldBePinned) {
+				if ((leaf as any)?.pinned !== shouldBePinned) {
 					leaf.setPinned(shouldBePinned);
 				}
 			}
