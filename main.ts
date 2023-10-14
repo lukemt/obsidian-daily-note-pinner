4a700d92a4fdc81be2f5de90e00c3dc117105d9e
Lukas Petry <git.luke@rennwurst.de>
2023-10-15 01:17:42 +0200

    :big: Use local timezoned date

--- main.ts
+++ main.ts
@@ -11,11 +11,17 @@ export default class DailyNotePinnerPlugin extends Plugin {
 	}
 
 	pinDailyNote = () => {
-		const today = new Date().toISOString().substring(0, 10);
+		const date = new Date();
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+		const today = `${year}-${month}-${day}`;
+
 		this.app.workspace.iterateRootLeaves(leaf => {
 			if (leaf.view instanceof FileView && dateRegex.test(leaf.view.file?.basename ?? "")) {
 				const shouldBePinned = leaf.view.file?.basename === today;
-				if ((leaf as any)?.pinned !== shouldBePinned) {
+				// @ts-ignore
+				if (leaf.pinned !== shouldBePinned) {
 					leaf.setPinned(shouldBePinned);
 				}
 			}
